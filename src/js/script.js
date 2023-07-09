495acb9f5463cf7351e90555ec39ee3ab068f778
Manas A <112828012+manasadepu@users.noreply.github.com>
2023-07-09 13:37:18 -0400

    Initialized project

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+let summarizebutton = document.getElementById("Summarize");
+
+summarizebutton.addEventListener("click", changeButton)
+
+function changeButton () {
+    summarizebutton.textContent = "Button has been clicked"
+}
